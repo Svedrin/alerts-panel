68e8cfd66a6366b787e2bf278e042032ce93def9
Michael Ziegler <michael.ziegler@proemion.com>
2019-09-05 20:59:58 +0200

    some minor refactoring :trollface:

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.use(express.static('src'))
+app.use(express.static('node_modules'))
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
