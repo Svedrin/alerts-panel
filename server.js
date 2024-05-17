58cf82d527aec1984a89742476ff94ee6b971d25
Michael Ziegler <diese-addy@funzt-halt.net>
2024-05-17 20:18:25 +0200

    add a silenced (acked) alert and one that pops up only every five minutes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('node_modules'))
 
 // Dummy endpoint used for testing -- shadow this in your reverse proxy
 app.get('/alert-manager/api/v1/alerts', function (req, res) {
-  res.send(JSON.stringify({
+  var data = {
     "status": "success",
     "data": [ {
       "labels": {
@@ -39,7 +39,7 @@ app.get('/alert-manager/api/v1/alerts', function (req, res) {
       },
       "annotations": {
         "description": "onehost: Systemd unit other.service has been in state \"failed\" for 15 minutes.",
-        "summary": "Systemd unit other.service is failed"
+        "summary": "Systemd unit other.service was failed but is good now"
       },
       "startsAt": new Date( (new Date()).getTime() - 300000 ).toISOString(), // Five minutes ago
       "endsAt":   new Date( (new Date()).getTime() -  60000 ).toISOString(), // A minute ago
@@ -82,8 +82,47 @@ app.get('/alert-manager/api/v1/alerts', function (req, res) {
         "silencedBy": [],
         "inhibitedBy": []
       }
+    }, {
+      "labels": {
+        "instance": "otherhost",
+        "service": "mail",
+        "severity": "disaster",
+      },
+      "annotations": {
+        "description": "otherhost: The mail server has been unreachable for 1 hour.",
+        "summary": "Our mail server is down but we know"
+      },
+      "startsAt": new Date( (new Date()).getTime() - 60000 ).toISOString(), // A minute ago
+      "endsAt":   new Date( (new Date()).getTime() + 60000 ).toISOString(), // A minute from now
+      "status": {
+        "state": "active",
+        "silencedBy": ["some-fake-uuid"],
+        "inhibitedBy": []
+      }
     } ]
-  }))
+  };
+  if ((new Date()).getMinutes() % 5 == 0) {
+    // Every 5 minutes, include another alert to see if the "alert fixed" detection works
+    data.data.push({
+      "labels": {
+        "instance": "thirdhost",
+        "service": "time",
+        "severity": "average",
+      },
+      "annotations": {
+        "description": "thirdhost: The current minute is divisible by five.",
+        "summary": "The current minute is divisible by five"
+      },
+      "startsAt": new Date( (new Date()).getTime() - 60000 ).toISOString(), // A minute ago
+      "endsAt":   new Date( (new Date()).getTime() + 60000 ).toISOString(), // A minute from now
+      "status": {
+        "state": "active",
+        "silencedBy": [],
+        "inhibitedBy": []
+      }
+    });
+  }
+  res.send(JSON.stringify(data));
 })
 
 app.listen(PORT, HOST);
